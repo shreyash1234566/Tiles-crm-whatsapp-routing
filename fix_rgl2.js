86c80217a39535a04221ce00c2ff4ffc0dc0ef7f
shreyash1234566 <ayushshreyash1@gmail.com>
2026-08-26 16:13:08 +0530

    Fix typescript and syntax errors; remove obsolete groups directory; fix react-grid-layout

--- fix_rgl2.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs')
-let file = fs.readFileSync('components/dashboard/WidgetGrid.tsx', 'utf8')
-file = file.replace(/import { Responsive, WidthProvider } from 'react-grid-layout'/, `// @ts-ignore\nimport { Responsive, WidthProvider } from 'react-grid-layout'`)
-fs.writeFileSync('components/dashboard/WidgetGrid.tsx', file)
--- patch_widgetgrid.sh
+++
@@ -1,3 +0,0 @@
-sed -i '/import { WidthProvider/d' components/dashboard/WidgetGrid.tsx
-sed -i 's/import {.*Responsive.*} from .react-grid-layout./import RGL from '\''react-grid-layout'\''\nconst { Responsive, WidthProvider } = RGL/g' components/dashboard/WidgetGrid.tsx
-sed -i "s/import type { Layout as CoreLayout } from 'react-grid-layout'/type CoreLayout = RGL.Layout/" components/dashboard/WidgetGrid.tsx
